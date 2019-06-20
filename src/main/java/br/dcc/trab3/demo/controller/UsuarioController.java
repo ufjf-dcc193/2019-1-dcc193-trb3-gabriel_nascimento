d35a07393c3bccf86d13ec4855a896dcb079fe83
gabrielnascimento95 <gabriel.nascimento@ice.ufjf.br>
2019-06-20 13:28:19 -0300

    criacao da pagina home

---
+++ src/main/java/br/dcc/trab3/demo/controller/UsuarioController.java
@@ -0,0 +1,14 @@
+package br.dcc.trab3.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * UsuarioController
+ */
+@Controller
+@RequestMapping("/avaliador/")
+public class UsuarioController {
+
+    
+}
--- src/main/java/br/dcc/trab3/demo/model/Item.java
+++ src/main/java/br/dcc/trab3/demo/model/Item.java
@@ -26,4 +26,49 @@ public class Item {
     private List<Anotacao> listaAnotacao;
     @OneToMany
     private List<Vinculo> listaVinculo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public List<Etiqueta> getListaEtiqueta() {
+        return listaEtiqueta;
+    }
+
+    public void setListaEtiqueta(List<Etiqueta> listaEtiqueta) {
+        this.listaEtiqueta = listaEtiqueta;
+    }
+
+    public List<Anotacao> getListaAnotacao() {
+        return listaAnotacao;
+    }
+
+    public void setListaAnotacao(List<Anotacao> listaAnotacao) {
+        this.listaAnotacao = listaAnotacao;
+    }
+
+    public List<Vinculo> getListaVinculo() {
+        return listaVinculo;
+    }
+
+    public void setListaVinculo(List<Vinculo> listaVinculo) {
+        this.listaVinculo = listaVinculo;
+    }
+
+    public Item() {
+    }
+
+    
 }
