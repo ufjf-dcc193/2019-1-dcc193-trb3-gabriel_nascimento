1369a34921eda72ed8712a75ebd7d088476b5a07
gabrielnascimento95 <gabriel.nascimento@ice.ufjf.br>
2019-06-20 14:00:21 -0300

    adcionando dados diretamente na bean

--- src/main/java/br/dcc/trab3/demo/DemoApplication.java
+++ src/main/java/br/dcc/trab3/demo/DemoApplication.java
@@ -1,13 +1,28 @@
 package br.dcc.trab3.demo;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import br.dcc.trab3.demo.dao.UsuarioRepository;
+import br.dcc.trab3.demo.model.Usuario;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	@Autowired
+	UsuarioRepository usuarios;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@Bean
+	InitializingBean sendDatabase() {
+		return () -> {
+			usuarios.save(new Usuario(null, "Gabriel Nascimento", "DCC193", "Estudante de TI", "gabriel.nascimento@ice.ufjf.br"));
+		};}
 
 }
