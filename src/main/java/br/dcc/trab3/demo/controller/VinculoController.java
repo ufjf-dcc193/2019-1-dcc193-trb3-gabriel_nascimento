a76dbb6e8cd1cbaf515751e86da2a1d8a45b8832
Gabriel Nascimento <gabrielnascimenttto@outlook.com>
2019-06-24 14:19:23 -0300

    vinculo controller

--- src/main/java/br/dcc/trab3/demo/controller/VinculoController.java
+++ src/main/java/br/dcc/trab3/demo/controller/VinculoController.java
@@ -1,7 +1,11 @@
 package br.dcc.trab3.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.dcc.trab3.demo.dao.VinculoRepository;
 
 /**
  * VinculoController
@@ -10,5 +14,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/vinculo/")
 public class VinculoController {
 
+    @Autowired
+    VinculoRepository vinculos;
+
+    @RequestMapping("")
+    public String homeVinculo(Model model){
+        model.addAttribute("listVinculos", vinculos.findAll());
+        return "vinculo/vinculo-index";
+    }
+
+    
     
 }
