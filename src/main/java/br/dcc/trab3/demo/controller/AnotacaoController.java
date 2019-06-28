ec99b2b71dd14a8bfd7fd9843724ecead772406f
Gabriel Nascimento <gabrielnascimenttto@outlook.com>
2019-06-28 13:30:39 -0300

    crud anotacao concluido

--- src/main/java/br/dcc/trab3/demo/controller/AnotacaoController.java
+++ src/main/java/br/dcc/trab3/demo/controller/AnotacaoController.java
@@ -13,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -76,5 +78,46 @@ public class AnotacaoController {
         mv.setViewName("redirect:/anotacao/");
         return mv;
     }
+
+    @GetMapping("/editar/{id}")
+    public String editarRevisao(@PathVariable Long id, Model model, HttpSession session){
+        Calendar c = Calendar.getInstance();
+        List<Usuario> listUsuario = new ArrayList<>();
+        Usuario usuario = (Usuario) session.getAttribute("ativo");
+        listUsuario.add(usuario);
+        Anotacao anotacao = anotacoes.findById(id).get();
+        model.addAttribute("anotacao", anotacao);
+        model.addAttribute("listUsuario", listUsuario);
+        return "anotacao/anotacao-edit";
+    }
+
+    @PostMapping("/editar/{id}")
+    public ModelAndView editarAnotacaoSubmit(@Valid Anotacao anotacao, @PathVariable Long id, HttpSession session, BindingResult binding){
+        ModelAndView mv = new ModelAndView();
+        Calendar c = Calendar.getInstance();
+        Date data = c.getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        
+        if(binding.hasErrors()){
+            mv.setViewName("anotacao/anotacao-edit"); 
+            List<Usuario> listUsuario = new ArrayList<>();
+            Usuario usuario = (Usuario) session.getAttribute("ativo");
+            listUsuario.add(usuario);
+            mv.addObject("anotacao", anotacao);
+            mv.addObject("listUsuario", listUsuario);
+            return mv;
+        }
+        anotacao.setDataAlteracao(sdf.format(data));
+        mv.setViewName("redirect:/anotacao/"); 
+        anotacoes.save(anotacao);
+        return mv;
+    }
+
+    @RequestMapping("/deletar/{id}")
+    public String deletarAnotacao(@PathVariable Long id){
+        anotacoes.deleteById(id);
+        return "redirect:/anotacao/";
+    }
+
     
 }
